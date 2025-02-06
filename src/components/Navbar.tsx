01b75f693cbc393010945d93ef3cdce824627162
Malvin A <malvinaudriansyah77@gmail.com>
2025-02-06 14:54:25 +0700

    fix(navbar): navbar is now position: sticky, and padding fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,87 +30,83 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      <nav className="w-full max-w-full flex flex-col bg-white px-4 py-6 lg:px-16 lg:py-8 sticky top-0">
-        <div className="flex flex-row w-full max-w-[1240px] justify-between mx-auto">
+    <nav className="sticky top-0 z-50 w-full max-w-full flex flex-col bg-white px-4 lg:px-16">
+      <div className="flex flex-row w-full max-w-[1240px] justify-between mx-auto py-6 lg:py-8">
+        <Link href={"/"}>
+          <h1 className="font-bold text-[18px] text-[#F15025]">Spoon & Fork</h1>
+        </Link>
+
+        {/* Desktop Layout */}
+
+        <div className="hidden lg:flex flex-row gap-6">
           <Link href={"/"}>
-            <h1 className="font-bold text-[18px] text-[#F15025]">
-              Spoon & Fork
-            </h1>
-          </Link>
-
-          {/* Desktop Layout */}
-
-          <div className="hidden lg:flex flex-row gap-6">
-            <Link href={"/"}>
-              <button className="hover:text-[#F15025] transition-all duration-700">
-                Home
-              </button>
-            </Link>
-            <Link href={"/recipes"}>
-              <button className="hover:text-[#F15025] transition-all duration-700">
-                Recipes
-              </button>
-            </Link>
-            <Link href={"/meal-planner"}>
-              <button className="hover:text-[#F15025] transition-all duration-700">
-                Meal Planner
-              </button>
-            </Link>
-            <Link href={"/about"}>
-              <button className="hover:text-[#F15025] transition-all duration-700">
-                About
-              </button>
-            </Link>
-            <Link href={"/contact"}>
-              <button className="hover:text-[#F15025] transition-all duration-700">
-                Contact
-              </button>
-            </Link>
-          </div>
-
-          {/* Mobile Layout */}
-
-          <button
-            className="lg:hidden text-[#F15025] text-[24px]"
-            onClick={handleMenu}
-          >
-            <RxHamburgerMenu />
-          </button>
-        </div>
-        <div
-          className={`${
-            openMenu ? "flex flex-col lg:hidden text-center" : "hidden"
-          }`}
-        >
-          <Link href={"/"}>
-            <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            <button className="hover:text-[#F15025] transition-all duration-700">
               Home
             </button>
           </Link>
           <Link href={"/recipes"}>
-            <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            <button className="hover:text-[#F15025] transition-all duration-700">
               Recipes
             </button>
           </Link>
           <Link href={"/meal-planner"}>
-            <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            <button className="hover:text-[#F15025] transition-all duration-700">
               Meal Planner
             </button>
           </Link>
           <Link href={"/about"}>
-            <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            <button className="hover:text-[#F15025] transition-all duration-700">
               About
             </button>
           </Link>
           <Link href={"/contact"}>
-            <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            <button className="hover:text-[#F15025] transition-all duration-700">
               Contact
             </button>
           </Link>
         </div>
-      </nav>
-    </>
+
+        {/* Mobile Layout */}
+
+        <button
+          className="lg:hidden text-[#F15025] text-[24px]"
+          onClick={handleMenu}
+        >
+          <RxHamburgerMenu />
+        </button>
+      </div>
+      <div
+        className={`${
+          openMenu ? "flex flex-col lg:hidden text-center" : "hidden"
+        }`}
+      >
+        <Link href={"/"}>
+          <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            Home
+          </button>
+        </Link>
+        <Link href={"/recipes"}>
+          <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            Recipes
+          </button>
+        </Link>
+        <Link href={"/meal-planner"}>
+          <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            Meal Planner
+          </button>
+        </Link>
+        <Link href={"/about"}>
+          <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            About
+          </button>
+        </Link>
+        <Link href={"/contact"}>
+          <button className="hover:text-[#F15025] transition-all duration-700 py-2">
+            Contact
+          </button>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
